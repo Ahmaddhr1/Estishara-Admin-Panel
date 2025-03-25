c6eab946c25e3b3fd9682dc3d21fddf5a8e041a2
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2025-03-25 14:48:10 +0200

    adding two pages for sidebar

--- apestishara/utils/SideBarTabs.js
+++ apestishara/utils/SideBarTabs.js
@@ -6,13 +6,18 @@ import {
     ListCheck,
     Menu,
     X,
+    Pill,
+    Bell,
   } from "lucide-react";
 
 const tabs =[
     { label: 'Dashboard', path: '/dashboard',icon:<Home /> },
     { label: 'Pending Doctors', path: '/dashboard/pendingdoctors',icon:<TimerIcon />},
+    { label: 'Patients', path: '/dashboard/patients', icon:<Pill />},
     { label: 'Specialities', path: '/dashboard/specialities' ,icon:<ListCheck />},
+    { label: 'Notifications', path: '/dashboard/notifications', icon:<Bell />},
     { label: 'Admins', path: '/dashboard/admins',icon:<User /> },
+    
 ]
 
 export default tabs;
